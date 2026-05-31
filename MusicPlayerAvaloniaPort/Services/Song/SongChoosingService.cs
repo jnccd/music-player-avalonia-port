8f5fda10939a926aa5cf414212d3ff6a840b3bbf
Dobiko <kobidogao@yahoo.com>
2026-05-31 16:29:50 +0200

    Add SongChoosingService

---
+++ MusicPlayerAvaloniaPort/Services/Song/SongChoosingService.cs
@@ -0,0 +1,14 @@
+using MusicPlayerAvaloniaPort.Helpers;
+using MusicPlayerAvaloniaPort.Services.Song;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace MusicPlayerAvaloniaPort.Services;
+
+[RegisterImplementation(ServiceRegisterType.Singleton, typeof(SongChoosingService))]
+public class SongChoosingService
+{
+
+}
