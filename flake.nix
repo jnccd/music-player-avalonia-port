ab33b9def5eecd7b149ae7d5d705b738db78d36d
jnccd <kobidogao@outlook.com>
2026-06-09 19:12:09 +0200

    Add gui devshell

--- flake.nix
+++ flake.nix
@@ -3,17 +3,27 @@
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.11";
-    numtide-utils.url = "github:numtide/flake-utils";
-    jnccd-utils.url = "github:jnccd/nix-utils";
+    numtide-utils.url = {
+      url = "github:numtide/flake-utils";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    jnccd-utils.url = {
+      url = "github:jnccd/nix-utils";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
-  outputs = { self, nixpkgs, ... }@inputs:
-    (inputs.numtide-utils.lib.eachSystem [ "x86_64-linux" "aarch64-linux" ]
-      (system: {
-        devShells.default = inputs.jnccd-utils.lib.mkUnfrozenDotnetShell {
+  outputs =
+    { self, nixpkgs, ... }@inputs:
+    (inputs.numtide-utils.lib.eachSystem [ "x86_64-linux" "aarch64-linux" ] (system: {
+      devShells = rec {
+        gui = inputs.jnccd-utils.lib.mkUnfrozenDotnetShell {
           inherit system nixpkgs;
           dotnetVersion = "10.0";
           includeAndroidSdk = false;
         };
-      }));
+
+        default = gui;
+      };
+    }));
 }
