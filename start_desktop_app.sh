df4ef236299d2e7998a642d372f2d6783bcc03d0
jnccd <kobidogao@outlook.com>
2026-07-21 15:08:26 +0200

    Use NIXOS_JNCCD_GUI_STARTER_UNCHANGED var in starter script

--- start_desktop_app.sh
+++ start_desktop_app.sh
@@ -1,4 +1,11 @@
 #!/usr/bin/env bash
 cd ./MusicPlayerAvaloniaPort
-DB_PROVIDER=sqlite MUSIC_PLAYER_SQLITE_DB_PATH=./bin/Release/net10.0/ dotnet ef database update
-dotnet run -c Release
+
+if [ -z "$NIXOS_JNCCD_GUI_STARTER_UNCHANGED" ] || [ "$NIXOS_JNCCD_GUI_STARTER_UNCHANGED" != "1" ]; then
+  # Changed - run database update and rebuild
+  DB_PROVIDER=sqlite MUSIC_PLAYER_SQLITE_DB_PATH=./bin/Release/net10.0/ dotnet ef database update
+  dotnet run -c Release
+else
+  # Unchanged - just run the existing DLL with DB vars
+  DB_PROVIDER=sqlite MUSIC_PLAYER_SQLITE_DB_PATH=./bin/Release/net10.0/ dotnet ./bin/Release/net10.0/MusicPlayerAvaloniaPort.dll
+fi
