d138a0915a0deb95ad02c29fa0622366f0b93694
Dobiko <kobidogao@yahoo.com>
2026-08-01 18:54:33 +0200

    Improve magic numbers

--- MusicPlayerAvaloniaPort/Services/Visualization/DiagramDataMapperService.cs
+++ MusicPlayerAvaloniaPort/Services/Visualization/DiagramDataMapperService.cs
@@ -12,9 +12,9 @@ namespace MusicPlayerAvaloniaPort.Services.Visualization;
 [RegisterImplementation(ServiceRegisterType.Singleton, typeof(DiagramDataMapperService))]
 public class DiagramDataMapperService(AudioLibWrapperService audioLibWrapperService, SongPlaybackService songPlaybackService, DbWrapperService dbWrapperService)
 {
-    private const double FFT_WINDOW_PERCENT_CHOPPED_BEGINNING = 0.005;
+    private const double FFT_WINDOW_PERCENT_CHOPPED_BEGINNING = 0.003;
     private const double FFT_WINDOW_PERCENT_CHOPPED_END = 0.6;
-    private const float FFT_WINDOW_VALUE_DIVISOR = 6500;
+    private const float FFT_WINDOW_VALUE_DIVISOR = 9001;
     private const double FFT_SAMPLES_HAMMING_WINDOW_DOWNWARD_EXPONENT = 2;
 
     private float[]? smoothedData;
