a55ee168f2a7d0224d776a011817d7f89fc83ee1
jnccd <kobidogao@outlook.com>
2026-09-08 21:52:03 +0200

    Use mkDotnetWithWorkloadsShell instead

--- flake.nix
+++ flake.nix
@@ -18,28 +18,35 @@
       system:
       let
         pkgs = import nixpkgs { inherit system; };
+        workloadsHashX86_64Linux = "sha256-AcfemNC9S9Lk9AeW+EokaKYJpf3aDGywMTsi821Mo9M=";
       in
       {
         devShells = rec {
-          desktop = inputs.jnccd-utils.lib.mkUnfrozenDotnetShell {
+          desktop = inputs.jnccd-utils.lib.mkDotnetWithWorkloadsShell {
             inherit system nixpkgs;
             dotnetVersion = "10.0";
             includeAndroidSdk = false;
 
             additionalPackages = [ pkgs.yt-dlp ];
-            command = "cd music-player-avalonia-port ; bash ./start_desktop_app.sh";
+
+            workloadsHash = workloadsHashX86_64Linux;
           };
 
-          dev = inputs.jnccd-utils.lib.mkUnfrozenDotnetShell {
+          dev = inputs.jnccd-utils.lib.mkDotnetWithWorkloadsShell {
             inherit system nixpkgs;
             dotnetVersion = "10.0";
             includeAndroidSdk = false;
 
-            additionalPackages = [ pkgs.pulseaudio pkgs.yt-dlp ];
-            preExecBash = ''
+            additionalPackages = [
+              pkgs.pulseaudio
+              pkgs.yt-dlp
+            ];
+            extraShellHook = ''
               export LD_LIBRARY_PATH="${pkgs.pulseaudio}/lib/:$LD_LIBRARY_PATH"
               export PULSE_SERVER=unix:/run/user/$(id -u)/pulse/native
             '';
+
+            workloadsHash = workloadsHashX86_64Linux;
           };
 
           default = dev;
